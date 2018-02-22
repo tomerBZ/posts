bd9858b32a81ea64a791162cb12635c04a01991d
Tomer David Ben Zohar <tomer@wisegroup.co.il>
2018-02-22 16:45:31 +0200

    fix for live app

--- posts/settings.py
+++ posts/settings.py
@@ -118,10 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#      os.path.join(BASE_DIR, "static"),
+# ]
+
+# All settings common to all environments
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 REST_FRAMEWORK = {
