a5c2da89be83bb4ba33507e1b53df7f60006c53d
Tomer David Ben Zohar <tomer@wisegroup.co.il>
2018-02-22 20:36:10 +0200

    Heroku Fixes

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve
+from posts import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('post.urls')),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT, 'show_indexes': False, }),
 ]
