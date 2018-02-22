83b208ad65f2ba91384b05cd712b367c818778da
Tomer David Ben Zohar <tomer@wisegroup.co.il>
2018-02-22 21:33:49 +0200

    No posts edge case

--- posts/settings.py
+++ posts/settings.py
@@ -57,11 +57,11 @@ WSGI_APPLICATION = 'posts.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dfrs1ufa24k3o3',
-        'USER': 'qunzqxysaoreew',
-        'PASSWORD': 'ef7d1acdef3c84d16892b7f5ea80f49e00c179b2bb2ae7e8a493dd71a0bafd74',
-        'HOST': 'ec2-79-125-110-209.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
+        'NAME': 'parkam-posts',
+        'USER': 'postgres',
+        'PASSWORD': '142267',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
